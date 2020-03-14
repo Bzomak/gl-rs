d1bc5b58bcaf30f7a02e35c0502dcff88e2426ac
Robert Horswell <r.j.horswell@googlemail.com>
2020-03-14 15:14:36 +0000

    Make all constants uppercase
    Breaking! While most constants were already uppercase, some were not (e.g. FLOAT_MAT3x4). This conversion means we can remove the non_upper_case_globals attribute.

--- gl_generator/generators/mod.rs
+++ gl_generator/generators/mod.rs
@@ -46,13 +46,26 @@ pub fn gen_struct_name(api: Api) -> &'static str {
 
 /// This function generates a `const name: type = value;` item.
 pub fn gen_enum_item(enm: &Enum, types_prefix: &str, dest: &mut dyn io::Write) -> io::Result<()> {
-    writeln!(dest,
-        "#[allow(dead_code, non_upper_case_globals)] pub const {ident}: {types_prefix}{ty} = {value}{cast_suffix};",
-        ident = enm.ident,
-        types_prefix = if enm.ty == "&'static str" { "" } else { types_prefix },
-        ty = if enm.ty == "&'static str" { "&str" } else { &*enm.ty },
+    writeln!(
+        dest,
+        "#[allow(dead_code)] pub const {ident}: {types_prefix}{ty} = {value}{cast_suffix};",
+        ident = enm.ident.to_ascii_uppercase(),
+        types_prefix = if enm.ty == "&'static str" {
+            ""
+        } else {
+            types_prefix
+        },
+        ty = if enm.ty == "&'static str" {
+            "&str"
+        } else {
+            &*enm.ty
+        },
         value = enm.value,
-        cast_suffix = if enm.cast { format!(" as {}{}", types_prefix, enm.ty) } else { String::new() },
+        cast_suffix = if enm.cast {
+            format!(" as {}{}", types_prefix, enm.ty)
+        } else {
+            String::new()
+        },
     )
 }
 
